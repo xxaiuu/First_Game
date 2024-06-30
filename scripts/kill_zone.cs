82913fc04ae63a20052127a0438f7aa55f21f44d
Alex Liu <alex.stillcravingfood@gmail.com>
2024-06-29 18:52:27 -0700

    edited kill zone where when body touch kill zone, the bone collision gets deleted aka the player falls off the world

--- scripts/kill_zone.cs
+++ scripts/kill_zone.cs
@@ -5,17 +5,19 @@ using System;
 public partial class kill_zone : Area2D
 {
 	private Timer _timer;
-	
 	private void _on_body_entered(Node2D body)
 	{
 		GD.Print("You Died!");
+		Engine.TimeScale = 0.5;
 		_timer =  GetNode<Timer>("Timer");	//references the Timer Node
+		body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
 		_timer.Start();
 
 	}
 
 	private void _on_timer_timeout()
 	{
+		Engine.TimeScale = 1;
 		GetTree().ReloadCurrentScene();
 	}
 
