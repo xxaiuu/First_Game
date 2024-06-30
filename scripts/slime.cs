7d3f3721644d7d2cf671a4e39f06b1b48fdd01fa
Alex Liu <alex.stillcravingfood@gmail.com>
2024-06-29 17:55:31 -0700

    updated script such that the slime looks like it's moving back and forth - graphic change

--- scripts/slime.cs
+++ scripts/slime.cs
@@ -5,20 +5,22 @@ public partial class slime : Node2D
 {
 	private const float speed = 60f;
 	private RayCast2D RightRay, LeftRay;
+	private AnimatedSprite2D sprite;
 	private int direction = 1;
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-
 		RightRay = GetNode<RayCast2D>("RayCastRight");
 		LeftRay = GetNode<RayCast2D>("RayCastLeft");
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
 		if(RightRay.IsColliding())
 			direction = -1;
+			sprite.FlipH = true;	//flips artwork so it looks like the sprite is walking back and forth
 		if(LeftRay.IsColliding())
 			direction = 1;
+			sprite.FlipH = false;
 		Position = new Vector2(Position.X + (direction * speed * (float)delta), Position.Y);
 	}
 }
